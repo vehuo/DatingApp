e6d54413ea1759991a56b58ab7a7f72a8a60ea84
vehuo <vesa.huotarinen@gmail.com>
2021-01-28 22:45:21 +0200

    basic authentication implemented using JSON Web Tokens

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,14 +2,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -18,11 +17,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         // e.g. api/users/2
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
